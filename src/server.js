d1f84c6ae683f57bf64ea795dced9fdf181118b6
Artem Chupylko <achupylko@gmail.com>
2026-05-24 11:50:58 +0000

    add logging requests

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
+import pino from 'pino-http';
 
 import 'dotenv/config';
 
@@ -12,6 +13,24 @@ app.use(express.json());
 
 // Allows requests from any sources
 app.use(cors());
+
+// Middleware for logging requests
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.get('/notes', (response, request) => {
   response.status(200).json({
