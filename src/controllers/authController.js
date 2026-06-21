3718fbf3325f0dd0ba23ba1f05794ffaeaac72b8
Artem Chupylko <achupylko@gmail.com>
2026-06-21 18:16:01 +0000

    add user login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
+import { Session } from '../models/session.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -27,4 +28,31 @@ export const registerUser = async (req, res) => {
   setSessionCookies(res, newSession);
 
   res.status(201).json(newUser);
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if a user with this email exists
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  // Compare password hashes
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  // Delete the old user session
+  await Session.deleteOne({ userId: user._id });
+
+  // Create a new session
+  const newSession = await createSession(user._id);
+
+  // Pass the response object and session
+  setSessionCookies(res, newSession);
+
+  res.status(200).json(user);
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,14 @@
 import { celebrate } from 'celebrate';
 import { Router } from 'express';
-import { registerUser } from '../controllers/authController';
-import { registerUserSchema } from '../validations/authValidation.js';
+import { loginUser, registerUser } from '../controllers/authController';
+import {
+  loginUserSchema,
+  registerUserSchema,
+} from '../validations/authValidation.js';
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/ayth/login', celebrate(loginUserSchema), loginUser);
 
 export default router;
